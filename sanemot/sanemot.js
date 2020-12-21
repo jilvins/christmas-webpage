e79fe9afcbdae99e78a6d0315e72bb30ea3bdbe4
jilvins <59861669+jilvins@users.noreply.github.com>
2020-12-21 13:36:10 +0200

    Additional functionality

---
+++ sanemot/sanemot.js
@@ -0,0 +1,17 @@
+let poetryButton = document.getElementById("poetry-btn")
+let mainContainer = document.querySelector('.poetry-container')
+let defaultText = document.getElementById('default-text')
+
+poetryButton.addEventListener("click", getPoetry)
+
+async function getPoetry() {
+    let response = await fetch('/sanemot/pantini.json')
+    let poetry = await response.json()
+    let num = Math.floor(Math.random() * poetry.length)
+    defaultText.innerHTML = poetry[num].dzejolis
+   
+}
+
+
+
+
